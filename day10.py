49a913f1ee8190efc343e70711be2de2736cd219
Kapit <olmak16@gmail.com>
2022-12-10 20:33:34 +0100

    day 10 in progress

--- day10.py
+++ day10.py
@@ -145,169 +145,195 @@ noop
 noop'''
 
 
-input_data = '''addx 15
-addx -11
-addx 6
-addx -3
-addx 5
-addx -1
-addx -8
-addx 13
-addx 4
-noop
-addx -1
-addx 5
-addx -1
-addx 5
-addx -1
-addx 5
-addx -1
-addx 5
-addx -1
-addx -35
-addx 1
-addx 24
-addx -19
-addx 1
-addx 16
-addx -11
-noop
-noop
-addx 21
-addx -15
-noop
-noop
-addx -3
-addx 9
-addx 1
-addx -3
-addx 8
-addx 1
-addx 5
-noop
-noop
-noop
-noop
-noop
-addx -36
-noop
-addx 1
-addx 7
-noop
-noop
-noop
-addx 2
-addx 6
-noop
-noop
-noop
-noop
-noop
-addx 1
-noop
-noop
-addx 7
-addx 1
-noop
-addx -13
-addx 13
-addx 7
-noop
-addx 1
-addx -33
-noop
-noop
-noop
-addx 2
-noop
-noop
-noop
-addx 8
-noop
-addx -1
-addx 2
-addx 1
-noop
-addx 17
-addx -9
-addx 1
-addx 1
-addx -3
-addx 11
-noop
-noop
-addx 1
-noop
-addx 1
-noop
-noop
-addx -13
-addx -19
-addx 1
-addx 3
-addx 26
-addx -30
-addx 12
-addx -1
-addx 3
-addx 1
-noop
-noop
-noop
-addx -9
-addx 18
-addx 1
-addx 2
-noop
-noop
-addx 9
-noop
-noop
-noop
-addx -1
-addx 2
-addx -37
-addx 1
-addx 3
-noop
-addx 15
-addx -21
-addx 22
-addx -6
-addx 1
-noop
-addx 2
-addx 1
-noop
-addx -10
-noop
-noop
-addx 20
-addx 1
-addx 2
-addx 2
-addx -6
-addx -11
-noop
-noop
-noop'''
+# input_data = '''addx 15
+# addx -11
+# addx 6
+# addx -3
+# addx 5
+# addx -1
+# addx -8
+# addx 13
+# addx 4
+# noop
+# addx -1
+# addx 5
+# addx -1
+# addx 5
+# addx -1
+# addx 5
+# addx -1
+# addx 5
+# addx -1
+# addx -35
+# addx 1
+# addx 24
+# addx -19
+# addx 1
+# addx 16
+# addx -11
+# noop
+# noop
+# addx 21
+# addx -15
+# noop
+# noop
+# addx -3
+# addx 9
+# addx 1
+# addx -3
+# addx 8
+# addx 1
+# addx 5
+# noop
+# noop
+# noop
+# noop
+# noop
+# addx -36
+# noop
+# addx 1
+# addx 7
+# noop
+# noop
+# noop
+# addx 2
+# addx 6
+# noop
+# noop
+# noop
+# noop
+# noop
+# addx 1
+# noop
+# noop
+# addx 7
+# addx 1
+# noop
+# addx -13
+# addx 13
+# addx 7
+# noop
+# addx 1
+# addx -33
+# noop
+# noop
+# noop
+# addx 2
+# noop
+# noop
+# noop
+# addx 8
+# noop
+# addx -1
+# addx 2
+# addx 1
+# noop
+# addx 17
+# addx -9
+# addx 1
+# addx 1
+# addx -3
+# addx 11
+# noop
+# noop
+# addx 1
+# noop
+# addx 1
+# noop
+# noop
+# addx -13
+# addx -19
+# addx 1
+# addx 3
+# addx 26
+# addx -30
+# addx 12
+# addx -1
+# addx 3
+# addx 1
+# noop
+# noop
+# noop
+# addx -9
+# addx 18
+# addx 1
+# addx 2
+# noop
+# noop
+# addx 9
+# noop
+# noop
+# noop
+# addx -1
+# addx 2
+# addx -37
+# addx 1
+# addx 3
+# noop
+# addx 15
+# addx -21
+# addx 22
+# addx -6
+# addx 1
+# noop
+# addx 2
+# addx 1
+# noop
+# addx -10
+# noop
+# noop
+# addx 20
+# addx 1
+# addx 2
+# addx 2
+# addx -6
+# addx -11
+# noop
+# noop
+# noop'''
 
 values ={
     'noop': 1,
     'addx': 2
 }
 
-screen = ['........................................',
-          '........................................',
-          '........................................',
-          '........................................',
-          '........................................',
-          '........................................']
 
-for line in range(len(screen)):
-    screen[line] = list(screen[line])
-screen_row = 0
+# x = 1
+# cycles = 0
+# data = None
+# looked_for = {20,60,100,140,180,220}
+# suma = 0
+# for line in input_data.splitlines():
+#     command = line.split(' ')[0]
+#     try:
+#         data = int(line.split(' ')[1])
+#     except IndexError:
+#         data = None
+#     for cycle in range(values[command]):
+#         cycles += 1
+#         if cycles in looked_for:
+#             suma+=x*cycles
+#
+#     if data is not None:
+#         x+= data
+#
+# print(suma)
 
+
+
+checker = ['........................................',
+          '........................................',
+          '........................................',
+          '........................................',
+          '........................................',
+          '........................................', ]
+
+for line in range(len(checker)):
+    checker[line] = list(checker[line])
+
+
+screen = ['']
+screen_line = 0
 x = 1
 cycles = 0
 data = None
@@ -319,23 +345,28 @@ for line in input_data.splitlines():
         data = int(line.split(' ')[1])
     except IndexError:
         data = None
+
     for cycle in range(values[command]):
         cycles += 1
-        print(x)
+        if x < 1:
+            x+=1
+        elif x == 39:
+            x-=1
+        print(checker[screen_line])
+        if checker[screen_line][x] == '.' or checker[screen_line][x-1] == '.' or checker[screen_line][x+1] == '.':
+            screen[screen_line] += '#'
+            checker[screen_line][x] = '#'
+            checker[screen_line][x-1] = '#'
+            checker[screen_line][x+1] = '#'
+        else:
+            screen[screen_line] += '.'
 
-        if x > 0 and x < 40:
-            screen[screen_row][x] = '#'
-        if x-1 > 0 and x-1 < 40:
-            screen[screen_row][x-1] = '#'
-        if x + 1 > 0 and x + 1 < 40:
-            screen[screen_row][x+1] = '#'
         if cycles in looked_for:
             suma+=x*cycles
-            screen_row+=1
+            screen_line +=1
+            screen.append('')
 
     if data is not None:
         x+= data
 
 print(screen)
-for line in screen:
-    print(''.join(line))
